dac7822705193b6255b4071617e5de89afaa0220
ceald1 <ceald@ceald.cc>
2026-03-25 14:30:19 -0700

    updates

--- cli/cli.go
+++ cli/cli.go
@@ -29,14 +29,14 @@ func Run() (result CLIResults, err error) {
 
 		Example: `
 # Run it
-gohound --domain "test.com" --dc "127.0.0.1" --ldaps --kerb --adcs --user "test" --password "test"
+gohound --domain "test.com" --dc "127.0.0.1" --ldaps --kerb --user "test" --password "test"
 		`,
 		Run: func(c *cobra.Command, args []string) {
 			result.Username, _ = c.Flags().GetString("user")
 			result.Password, _ = c.Flags().GetString("password")
 			result.Kerberos, _ = c.Flags().GetBool("kerb")
 			result.Ldaps, _ = c.Flags().GetBool("ldaps")
-			result.ADCS, _ = c.Flags().GetBool("adcs")
+			//result.ADCS, _ = c.Flags().GetBool("adcs")
 			result.DC, _ = c.Flags().GetString("dc")
 			result.NtlmHash, _ = c.Flags().GetString("hash")
 			result.Port, _ = c.Flags().GetString("port")
@@ -47,7 +47,7 @@ gohound --domain "test.com" --dc "127.0.0.1" --ldaps --kerb --adcs --user "test"
 	cmd.Flags().String("user", "", "")
 	cmd.Flags().Bool("kerb", false, "")
 	cmd.Flags().Bool("ldaps", false, "")
-	cmd.Flags().Bool("adcs", false, "")
+	//cmd.Flags().Bool("adcs", false, "")
 	cmd.Flags().String("dc", "127.0.0.1", "")
 	cmd.Flags().String("hash", "", "")
 	cmd.Flags().String("port", "389", "")
